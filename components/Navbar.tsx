91a527d7bafb6774032d8876067a7e6b78910978
gnmjafari <gnm.jafaria@gmail.com>
2024-10-22 20:27:34 +0330

    Browse The Range

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const menuItem: string[] = ["Home", "Shop", "About", "Contact"];
 
   return (
-    <div className="navbar bg-white">
+    <div className="navbar relative bg-white">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
