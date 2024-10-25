ea25b0081bb2782a8abf690266b95d74d6b6d77a
gnmjafari <gnm.jafaria@gmail.com>
2024-10-26 00:13:00 +0330

    pagination list

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,14 @@
 import { products } from "@/components/data";
+import { type NextRequest } from "next/server";
 
-export function GET() {
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const page = searchParams.get("page");
+  const show = searchParams.get("show");
+  const shortBy = searchParams.get("shortBy");
+  console.log("page", page);
+  console.log("show", show);
+  console.log("shortBy", shortBy);
+
   return Response.json(products, { status: 200 });
 }
