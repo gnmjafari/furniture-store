57f33d43165ae4c8a1b160c0556c25ac7325c246
gnmjafari <gnm.jafaria@gmail.com>
2024-10-30 19:10:21 +0330

    show product list: single

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -14,7 +14,7 @@ const Shop: NextPage = () => {
   const [pagination, setPagination] = useState<number>(1);
   const [show, setShow] = useState<string>("8");
   const [shortBy, setShortBy] = useState<string>("New");
-  const [howDisplay, setHowDisplay] = useState<string>("group");
+  const [howDisplay, setHowDisplay] = useState<"group" | "single">("group");
 
   const { data, isLoading } = useSWR(
     `/api/?page=${pagination}&show=${show}&shortBy=${shortBy}`,
@@ -70,12 +70,12 @@ const Shop: NextPage = () => {
           </div>
           <div className="flex gap-1 justify-start items-center">
             <button
+              onClick={() => setHowDisplay("group")}
               className={`btn btn-ghost ${
-                howDisplay == "Group" && "btn-active"
+                howDisplay == "group" && "btn-active"
               }`}
             >
               <Image
-                onClick={() => setHowDisplay("Group")}
                 src="/icon/ci_grid-big-round.png"
                 alt="ci_grid-big-round"
                 width={20}
@@ -88,9 +88,13 @@ const Shop: NextPage = () => {
                 }}
               />
             </button>
-            <button className="btn btn-ghost">
+            <button
+              onClick={() => setHowDisplay("single")}
+              className={`btn btn-ghost ${
+                howDisplay == "single" && "btn-active"
+              }`}
+            >
               <Image
-                onClick={() => setHowDisplay("Single")}
                 src="/icon/bi_view-list.png"
                 alt="bi_view-list"
                 className="cursor-pointer"
@@ -142,7 +146,7 @@ const Shop: NextPage = () => {
           <ProductCardLoading />
         </div>
       ) : (
-        <ProductList products={data?.products} />
+        <ProductList products={data?.products} howDisplay={howDisplay} />
       )}
 
       <div className="flex justify-center items-center mt-10">
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,9 +4,13 @@ import Image from "next/image";
 
 type ProductCardProps = {
   item: Product;
+  howDisplay: "group" | "single";
 };
 
-const ProductCard: FunctionComponent<ProductCardProps> = ({ item }) => {
+const ProductCard: FunctionComponent<ProductCardProps> = ({
+  item,
+  howDisplay,
+}) => {
   const formattedPrice = item.price.toLocaleString("en");
 
   const calculateDiscountedPrice = (
@@ -18,69 +22,155 @@ const ProductCard: FunctionComponent<ProductCardProps> = ({ item }) => {
     return finalPrice.toLocaleString("en");
   };
 
-  return (
-    <div className="indicator ">
-      {item.discount ? (
-        <span className="indicator-item badge badge-error">
-          {item.discountPercentage}%
-        </span>
-      ) : (
-        item.new && (
-          <span className="indicator-item badge badge-accent">New</span>
-        )
-      )}
-      <div className="card relative cursor-pointer card-compact w-64 shadow-xl group/product overflow-hidden">
-        <figure>
-          <div className="relative max-w-[285px] max-h-[301px] min-w-[285px] min-h-[301px] ">
-            <Image
-              className="object-cover"
-              src={item.image}
-              alt={item.name}
-              layout="fill"
-              // width={285}
-              // height={301}
-            />
-          </div>
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{item.name}</h2>
-          <p>{item.description}</p>
-          <div className="flex justify-between items-center">
-            <p className="font-bold text-md">
-              Rp {calculateDiscountedPrice(item.price, item.discountPercentage)}
-            </p>
-            {item.discount && (
-              <p className="font-bold text-md line-through text-gray-500">
-                Rp {formattedPrice}
-              </p>
-            )}
-          </div>
-        </div>
-        <div className="absolute invisible p-2 flex-col gap-3  group-hover/product:visible left-0 top-0 bottom-0 right-0  bg-[#3a3a3ab2] flex justify-center items-center">
-          <button className="btn  btn-outline btn-warning">Add to cart</button>
-          <div className="flex items-center justify-between gap-1">
-            <button className="btn btn-xs text-white btn-ghost gap-1">
-              <Image src="/icon/share.png" alt="share" width={10} height={10} />
-              Share
-            </button>
-            <button className="btn btn-xs text-white btn-ghost gap-1">
+  if (howDisplay == "single") {
+    return (
+      <div className="indicator w-3/4 md:w-3/4 lg:w-3/4">
+        {item.discount ? (
+          <span className="indicator-item badge badge-error">
+            {item.discountPercentage}%
+          </span>
+        ) : (
+          item.new && (
+            <span className="indicator-item badge badge-accent">New</span>
+          )
+        )}
+        <div className="card sm:card-side  bg-base-100 w-full shadow-xl group/product overflow-hidden">
+          <figure>
+            <div className="relative max-w-[285px] max-h-[301px] min-w-[285px] min-h-[301px] ">
               <Image
-                src="/icon/Compare.png"
-                alt="Compare"
-                width={12}
-                height={12}
+                className="object-cover"
+                src={item.image}
+                alt={item.name}
+                layout="fill"
               />
-              Compare
-            </button>{" "}
-            <button className="btn btn-xs text-white btn-ghost gap-1">
-              <Image src="/icon/Heart.png" alt="Heart" width={12} height={12} />
-              Like
+            </div>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{item.name}</h2>
+            <p>{item.description}</p>
+
+            <div className="card-actions justify-end">
+              <p className="font-bold text-md">
+                Rp{" "}
+                {calculateDiscountedPrice(item.price, item.discountPercentage)}
+              </p>
+            </div>
+          </div>
+          <div className="absolute invisible p-2 flex-col gap-3  group-hover/product:visible left-0 top-0 bottom-0 right-0  bg-[#3a3a3ab2] flex justify-center items-center">
+            <button className="btn  btn-outline btn-warning">
+              Add to cart
             </button>
+            <div className="flex items-center justify-between gap-1">
+              <button className="btn btn-xs text-white btn-ghost gap-1">
+                <Image
+                  src="/icon/share.png"
+                  alt="share"
+                  width={10}
+                  height={10}
+                />
+                Share
+              </button>
+              <button className="btn btn-xs text-white btn-ghost gap-1">
+                <Image
+                  src="/icon/Compare.png"
+                  alt="Compare"
+                  width={12}
+                  height={12}
+                />
+                Compare
+              </button>{" "}
+              <button className="btn btn-xs text-white btn-ghost gap-1">
+                <Image
+                  src="/icon/Heart.png"
+                  alt="Heart"
+                  width={12}
+                  height={12}
+                />
+                Like
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else if (howDisplay == "group") {
+    return (
+      <div className="indicator ">
+        {item.discount ? (
+          <span className="indicator-item badge badge-error">
+            {item.discountPercentage}%
+          </span>
+        ) : (
+          item.new && (
+            <span className="indicator-item badge badge-accent">New</span>
+          )
+        )}
+        <div className="card relative cursor-pointer card-compact w-64 shadow-xl group/product overflow-hidden">
+          <figure>
+            <div className="relative max-w-[285px] max-h-[301px] min-w-[285px] min-h-[301px] ">
+              <Image
+                className="object-cover"
+                src={item.image}
+                alt={item.name}
+                layout="fill"
+                // width={285}
+                // height={301}
+              />
+            </div>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{item.name}</h2>
+            <p>{item.description}</p>
+            <div className="flex justify-between items-center">
+              <p className="font-bold text-md">
+                Rp{" "}
+                {calculateDiscountedPrice(item.price, item.discountPercentage)}
+              </p>
+              {item.discount && (
+                <p className="font-bold text-md line-through text-gray-500">
+                  Rp {formattedPrice}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="absolute invisible p-2 flex-col gap-3  group-hover/product:visible left-0 top-0 bottom-0 right-0  bg-[#3a3a3ab2] flex justify-center items-center">
+            <button className="btn  btn-outline btn-warning">
+              Add to cart
+            </button>
+            <div className="flex items-center justify-between gap-1">
+              <button className="btn btn-xs text-white btn-ghost gap-1">
+                <Image
+                  src="/icon/share.png"
+                  alt="share"
+                  width={10}
+                  height={10}
+                />
+                Share
+              </button>
+              <button className="btn btn-xs text-white btn-ghost gap-1">
+                <Image
+                  src="/icon/Compare.png"
+                  alt="Compare"
+                  width={12}
+                  height={12}
+                />
+                Compare
+              </button>{" "}
+              <button className="btn btn-xs text-white btn-ghost gap-1">
+                <Image
+                  src="/icon/Heart.png"
+                  alt="Heart"
+                  width={12}
+                  height={12}
+                />
+                Like
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ProductCard;
